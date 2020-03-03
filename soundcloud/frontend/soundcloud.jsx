4ddff9d3440b3d508fb0100c31ea636bbbba2ce1
Steph Meyering <stephane.meyering@gmail.com>
2020-03-03 09:30:36 -0800

    completed all user auth reducers

--- soundcloud/frontend/soundcloud.jsx
+++ soundcloud/frontend/soundcloud.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login } from './util/session_api_util';
 import configureStore from './store/store';
-import Root from './components/root';
+// import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+
+    // testing start
     const store = configureStore();
-    ReactDOM.render(<Root store={store} />, root);
+    window.signup = signup;
+    window.login = login;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch; 
+    // testing end
+    
+    ReactDOM.render(<h1>hola</h1>, root);
 });
