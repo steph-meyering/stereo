53fd530f360847fbacc461004d72b6e3ed861a24
Steph Meyering <stephane.meyering@gmail.com>
2020-03-04 20:53:50 -0800

    fixed heroku db setup issues

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -10,7 +10,6 @@ const App = () => (
     <div>
         <ModalContainer />
         <header>
-            <h2>Hello</h2>
             <h1>(((Stereo)))</h1>
             <GreetingContainer />
         </header>
