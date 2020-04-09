51a237631da1e4782c9d9705384729c3af6ec088
Steph Meyering <stephane.meyering@gmail.com>
2020-04-09 15:51:05 -0700

    refactor navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,29 +4,48 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ currentUser, logout, openModal}) => {
     if (currentUser) {
         return (
-            <div className='navbar-div'>
-                <Link to='/'><div className='stereo-icon-div'>
-                    <h1 className='stereo-icon'>(((Stereo)))</h1>
-                </div></Link>
-                <div className='navbar-buttons'>
-                    <button className='sign-out-button' onClick={() => logout()}>Sign out</button>
-                    <Link to={`/users/${currentUser.id}`}> my profile</Link>
+          <div className="navbar-div">
+            <div className='navbar-content'>
+              <Link to="/">
+                <div className="stereo-icon-div">
+                  <h1 className="stereo-icon">(((Stereo)))</h1>
                 </div>
+              </Link>
+              <div className="navbar-buttons">
+                <button className="sign-out-button" onClick={() => logout()}>
+                  Sign out
+                </button>
+                <Link to={`/users/${currentUser.id}`}> my profile</Link>
+              </div>
             </div>
-        )
+          </div>
+        );
     } else {
         return (
-            <div className='navbar-div'>
-                <Link to='/'><div className='stereo-icon-div'>
-                    <h1 className='stereo-icon'>(((Stereo)))</h1>
-                </div></Link>
-                <div className='navbar-buttons'>
-                    <button className='sign-in-button' onClick={()=> openModal('login')}>Sign in</button>
-                    <button className='create-account-button' onClick={() => openModal('signup')}>Create account</button>
+          <div className="navbar-div">
+            <div className="navbar-content">
+              <Link to="/">
+                <div className="stereo-icon-div">
+                  <h1 className="stereo-icon">(((Stereo)))</h1>
                 </div>
+              </Link>
+              <div className="navbar-buttons">
+                <button
+                  className="sign-in-button"
+                  onClick={() => openModal("login")}
+                >
+                  Sign in
+                </button>
+                <button
+                  className="create-account-button"
+                  onClick={() => openModal("signup")}
+                >
+                  Create account
+                </button>
+              </div>
             </div>
-
-        )
+          </div>
+        );
     }
 }
 
