029ce01edce444dcb754f66ff280c856a71c4f2a
Steph Meyering <stephane.meyering@gmail.com>
2020-04-09 22:04:08 -0700

    song upload form input styling

--- frontend/components/songs/song_form.jsx
+++ frontend/components/songs/song_form.jsx
@@ -54,59 +54,64 @@ class SongForm extends React.Component {
         console.log(this.state);
         const preview = this.state.photoUrl ? <img src={this.state.photoUrl} /> : <div className='place-holder-cover'></div>;
         return (
-            <div className='upload-background'>
-                <div className='song-upload-form-container'>
-                    <form
-                        onSubmit={this.handleSubmit}
-                    >
-                        <h3>Please select an audio file to upload </h3>
-                        <input
-                            className='audio-upload'
-                            type="file"
-                            onChange={this.handleFile}
-                        />
-                        <div className='song-upload-form'>
-                            <div className='cover-photo-div'>                    
-                                <div className='cover-preview'>
-                                    {preview}
-                                    <input
-                                        className='photo-upload'
-                                        type="file"
-                                        onChange={this.handlePhoto}
-                                    />
-                                </div>
-                            </div>
-                            <br/>
-                            <div className='fields-div'>
-                                <h3>Title *</h3>
-                                <input 
-                                    type="text"
-                                    value={this.state.title}
-                                    onChange={this.update('title')}
-                                />
-                                <br/>
-                                <h3>Genre </h3>
-                                <input
-                                    type="text"
-                                    value={this.state.genre}
-                                    onChange={this.update('genre')}
-                                />
-                                <br />
-                                {/* <h3>Artist ID </h3>
+          <div className="upload-background">
+            <div className="song-upload-form-container">
+              <form onSubmit={this.handleSubmit}>
+                <h3>Please select an audio file to upload </h3>
+                <input
+                  className="audio-upload"
+                  type="file"
+                  onChange={this.handleFile}
+                />
+                <div className="song-upload-form">
+                  <div className="cover-photo-div">
+                    <div className="cover-preview">
+                      {preview}
+                      <label className="image-upload-button" for="cover-photo-upload">
+                        Upload image
+                      </label>
+                      <input
+                        id="cover-photo-upload"
+                        className="hide"
+                        type="file"
+                        onChange={this.handlePhoto}
+                        accept="image/jpeg,image/pjpeg,image/gif,image/png"
+                      />
+                    </div>
+                  </div>
+                  <br />
+                  <div className="fields-div">
+                    <h3>Title *</h3>
+                    <input
+                      type="text"
+                      value={this.state.title}
+                      onChange={this.update("title")}
+                    />
+                    <br />
+                    <h3>Genre </h3>
+                    <input
+                      type="text"
+                      value={this.state.genre}
+                      onChange={this.update("genre")}
+                    />
+                    <br />
+                    {/* <h3>Artist ID </h3>
                                 <input
                                     type="text"
                                     value={this.state.artistId}
                                     onChange={this.update('artistId')}
                                 />
                                 <br/> */}
-                            </div>
-                        </div>
-                        <button className='cancel-upload' >Cancel</button>
-                        <button className='save-upload' type="submit" >Save</button>
-                    </form>
+                  </div>
                 </div>
+                <button className="cancel-upload">Cancel</button>
+                <button className="save-upload" type="submit">
+                  Save
+                </button>
+              </form>
             </div>
-        )
+          </div>
+        );
     }
 }
 
