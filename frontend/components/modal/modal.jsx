1d628dc04bdc35b1d92f74eb0a01d08e9561506b
Steph Meyering <stephane.meyering@gmail.com>
2020-04-23 22:23:25 -0700

    add react-reveal package, make play controls appear when a song is loaded

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
+import React from "react";
+import Bounce from "react-reveal/Bounce";
 import LoginFormContainer from "../session_form/login_form_container";
 import SignupFormContainer from "../session_form/signup_form_container";
-import SongEdit from '../songs/song_edit';
+import SongEdit from "../songs/song_edit";
 
 function Modal({ modal, closeModal }) {
-    if (!modal) {
-        return null;
-    }
-    let component;
-    switch (modal) {
-        case 'login':
-            component = <LoginFormContainer />;
-            break;
-        case 'signup':
-            component = <SignupFormContainer />;
-            break;
-        case 'edit-song':
-            component = <SongEdit />;
-            break;
-        default:
-            return null;
-    }
-    return (
-        <div className="modal-background" onClick={closeModal}>
-            <div className="modal-child" onClick={e => e.stopPropagation()}>
-                {component}
-            </div>
+  if (!modal) {
+    return null;
+  }
+  let component;
+  switch (modal) {
+    case "login":
+      component = <LoginFormContainer />;
+      break;
+    case "signup":
+      component = <SignupFormContainer />;
+      break;
+    case "edit-song":
+      component = <SongEdit />;
+      break;
+    default:
+      return null;
+  }
+  return (
+    <div className="modal-background" onClick={closeModal}>
+      <Bounce top exit='true'>
+        <div className="modal-child" onClick={(e) => e.stopPropagation()}>
+          {component}
         </div>
-    );
+      </Bounce>
+    </div>
+  );
 }
 
 export default Modal;
-
--- frontend/components/play_controls/play_controls.jsx
+++ frontend/components/play_controls/play_controls.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
+import Slide from "react-reveal/Slide";
+
 
 class PlayControls extends React.Component {
     render(){
         if (this.props.currentSong === null) {
-            return (
-                <span id='play-controls'>
-                    <audio controls src="#"></audio>
-                </span>
-            )    
+            return null
         } else {
             return (
-                <span id='play-controls'> 
-                    <audio controls autoPlay src={this.props.currentSong.fileUrl}></audio>
-                    <div className='currently-playing-song-data'>
-                        <div>
-                            <img src={this.props.currentSong.photoUrl} alt={this.props.currentSong.title} />
+                <Slide bottom>
+                    <span id='play-controls'> 
+                        <audio controls autoPlay src={this.props.currentSong.fileUrl}></audio>
+                        <div className='currently-playing-song-data'>
+                            <div>
+                                <img src={this.props.currentSong.photoUrl} alt={this.props.currentSong.title} />
+                            </div>
+                            <div>
+                                <p className='uploader'>{this.props.currentSong.artist}</p>
+                                <p className='song-title'>{this.props.currentSong.title}</p>
+                            </div>
                         </div>
-                        <div>
-                            <p className='uploader'>{this.props.currentSong.artist}</p>
-                            <p className='song-title'>{this.props.currentSong.title}</p>
-                        </div>
-                    </div>
-                </span>
+                    </span>
+                </Slide>
             )
         }
     }
