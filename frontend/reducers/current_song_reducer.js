8a0a8b2aaf8a06dba5daea3168f382d569e8c647
Steph Meyering <stephane.meyering@gmail.com>
2020-08-19 13:39:42 -0700

    bug fix - clear state seek info by default unless action is 'SEEK'

--- frontend/reducers/current_song_reducer.js
+++ frontend/reducers/current_song_reducer.js
@@ -1,5 +1,5 @@
 import { SELECT_SONG, PLAY_PAUSE_SONG, SEEK, SEEK_CLEAR } from "../actions/current_song_actions";
-import { RECEIVE_SONG } from "../actions/song_actions";
+import { RECEIVE_SONG, RECEIVE_SONGS } from "../actions/song_actions";
 
 const currentSongReducer = (state = null, action) => {
   Object.freeze(state);
@@ -14,26 +14,24 @@ const currentSongReducer = (state = null, action) => {
       nextState["playing"] = true;
       nextState["seek"] = false;
       return nextState;
-    case RECEIVE_SONG:
-      // reset any existing seek state when receiving a song, not doing so can cause error
-      if (nextState["seek"]){
-        nextState["seek"] = false;
-        return nextState;
-      } else {
-        return state
-      }
     case PLAY_PAUSE_SONG:
       nextState["playing"] = nextState["playing"] === true ? false : true;
       nextState["seek"] = false;
       return nextState;
-    case SEEK: 
+    case SEEK:
       nextState["seek"] = {
         origin: action.origin,
-        position: action.position
+        position: action.position,
       };
       return nextState;
     default:
-      return state;
+      // reset any existing "seek" state,
+      if (nextState["seek"]) {
+        nextState["seek"] = false;
+        return nextState;
+      } else {
+        return state;
+      }
   }
 };
 
