5f90865211a51f11baaa45a8ad944917c6713114
Steph Meyering <stephane.meyering@gmail.com>
2020-08-31 20:33:09 -0700

    add redirect to profile page on successful login

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,61 +1,79 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
-const Navbar = ({ currentUser, logout, openModal }) => {
-  if (currentUser) {
-    return (
-      <div className="navbar-div">
-        <div className="navbar-content">
-          <Link to="/">
-            <div className="stereo-icon-div">
-              <h1 className="stereo-icon">(((Stereo)))</h1>
-            </div>
-          </Link>
-          <div className="navbar-buttons">
-            <button className="sign-out-button" onClick={() => logout()}>
-              Sign out
-            </button>
-            <Link className="upload-button" to="/upload">
-              Upload
-            </Link>
-            <Link to={`/users/${currentUser.id}`}>
-              <img
-                className="profile-pic-thumbnail"
-                src="https://i.pinimg.com/originals/8f/15/5c/8f155c9323941657c157c3ce8e4df589.jpg"
-                alt="profile-picture"
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="navbar-div">
-        <div className="navbar-content">
-          <Link to="/">
-            <div className="stereo-icon-div">
-              <h1 className="stereo-icon">(((Stereo)))</h1>
-            </div>
-          </Link>
-          <div className="navbar-buttons">
-            <button
-              className="sign-in-button"
-              onClick={() => openModal("login")}
-            >
-              Sign in
-            </button>
-            <button
-              className="create-account-button"
-              onClick={() => openModal("signup")}
-            >
-              Create account
-            </button>
-          </div>
-        </div>
-      </div>
-    );
+class Navbar extends React.Component {
+  constructor(props){
+    super(props)
+    this.redirect = false;
   }
-};
+  
+  render(){
+    const {currentUser, logout, openModal, history} = this.props
+    if (currentUser) {
+      if (this.redirect) {
+        this.redirect = false;
+        return(<Redirect to={`/users/${currentUser.id}`} />)
+      }
+      return (
+        <div className="navbar-div">
+          <div className="navbar-content">
+            <Link to="/">
+              <div className="stereo-icon-div">
+                <h1 className="stereo-icon">(((Stereo)))</h1>
+              </div>
+            </Link>
+            <div className="navbar-buttons">
+              <button className="sign-out-button" onClick={() => {
+                logout();
+                this.redirect = true;
+              }}>
+                Sign out
+              </button>
+              <Link className="upload-button" to="/upload">
+                Upload
+              </Link>
+              <Link to={`/users/${currentUser.id}`}>
+                <img
+                  className="profile-pic-thumbnail"
+                  src="https://i.pinimg.com/originals/8f/15/5c/8f155c9323941657c157c3ce8e4df589.jpg"
+                  alt="profile-picture"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="navbar-div">
+          <div className="navbar-content">
+            <Link to="/">
+              <div className="stereo-icon-div">
+                <h1 className="stereo-icon">(((Stereo)))</h1>
+              </div>
+            </Link>
+            <div className="navbar-buttons">
+              <button
+                className="sign-in-button"
+                onClick={() => {
+                  this.redirect = true;
+                  openModal("login")
+                }}
+              >
+                Sign in
+              </button>
+              <button
+                className="create-account-button"
+                onClick={() => openModal("signup")}
+              >
+                Create account
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
+}
 
 export default Navbar;
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,117 +1,117 @@
-import React from 'react';
+import React from "react";
 
 class SessionForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            email: '',
-            location: '',
-            // artist: 'true'
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      email: "",
+      location: "",
+      // artist: 'true'
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
+  update(field) {
+    return (e) =>
+      this.setState({
+        [field]: e.currentTarget.value,
+      });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user)
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  signupFields() {
+    if (this.props.formType === "signup") {
+      return (
+        <div>
+          <br />
+          <h3>Email:</h3>
+          <input
+            type="text"
+            value={this.state.email}
+            onChange={this.update("email")}
+          />
+        </div>
+      );
     }
+  }
 
-    update(field) {
-        return e => this.setState({
-            [field]: e.currentTarget.value
-        });
+  signupGreeting() {
+    if (this.props.formType === "signup") {
+      return <h1>Create your account on Stereo</h1>;
     }
+  }
 
-    
-    handleSubmit(e) {
-        e.preventDefault();
-        const user = Object.assign({}, this.state);
-        this.props.processForm(user);
+  guestUserLoginButton() {
+    if (this.props.formType === "login") {
+      return (
+        <div>
+          <br />
+          <button
+            type="button"
+            onClick={() => {
+              this.props.guestLogin();
+            }}
+          >
+            Demo User Login
+          </button>
+        </div>
+      );
     }
+  }
 
-    renderErrors() {
-        return (
-            <ul>
-                {this.props.errors.map((error, i) => (
-                    <li key={`error-${i}`}>
-                        {error}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit} className="login-form-box">
+          {this.signupGreeting()}
+          <br />
+          {/* Please {this.props.formType}, or {this.props.otherForm} */}
+          {this.renderErrors()}
+          {this.guestUserLoginButton()}
+          <div className="login-form">
+            <br />
+            <h3>Username:</h3>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.update("username")}
+            />
 
-    signupFields() {
-        if (this.props.formType === 'signup')
-        {    
-            return(
-                <div>
-                    <br/>
-                    <h3>Email:</h3>
-                    <input type="text"
-                            value={this.state.email}
-                            onChange={this.update('email')}
-                    />
-                </div>
-            )
-        }
-    }
+            <br />
+            <h3>Password: </h3>
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.update("password")}
+            />
+            {this.signupFields()}
 
-    signupGreeting() {
-        if (this.props.formType === 'signup') {
-            return (
-                <h1>Create your account on Stereo</h1>
-            )
-        }
-    }
-
-    guestUserLoginButton() {
-        if (this.props.formType === 'login') {
-            return (
-                <div>
-                    <br/>
-                    <button 
-                        type="button" 
-                        onClick={() => this.props.guestLogin()}>
-                            Guest User Login
-                    </button>
-                </div>
-            );
-        }
-    }
-    
-    render() {
-        return (
-            <div className="login-form-container">
-                <form onSubmit={this.handleSubmit} className="login-form-box">
-                {this.signupGreeting()}
-                <br />
-                    {/* Please {this.props.formType}, or {this.props.otherForm} */}
-                    {this.renderErrors()}
-                    {this.guestUserLoginButton()}
-                    <div className="login-form">
-
-                        <br />
-                        <h3>Username:</h3>
-                          <input type="text"
-                                value={this.state.username}
-                                onChange={this.update('username')}
-                          />
-                    
-
-                        <br />
-                        <h3>Password: </h3>
-                            <input type="password"
-                                value={this.state.password}
-                                onChange={this.update('password')}
-                            />
-                        {this.signupFields()}
-                 
-                        <br />
-                        <button className="session-submit" type="submit" >{this.props.formType}</button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+            <br />
+            <button className="session-submit" type="submit">
+              {this.props.formType}
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SessionForm;
