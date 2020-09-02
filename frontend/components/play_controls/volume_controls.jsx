f78d66ef2fcaebf52652b01114fb144847542a11
Steph Meyering <stephane.meyering@gmail.com>
2020-09-02 13:19:04 -0700

    add mute toggle and dynamic sound icon

--- frontend/components/play_controls/volume_controls.jsx
+++ frontend/components/play_controls/volume_controls.jsx
@@ -4,14 +4,29 @@ class VolumeControls extends React.Component {
   constructor(props) {
     super(props);
     this.selecting = false;
-    this.state = { 
+    this.dragEndOnMute = false;
+    this.lastVolume = 1;
+    this.state = {
       volume: 1,
     };
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     this.filledAxis.style.height = `${this.state.volume * 100}%`;
     this.audio.volume = this.state.volume;
+    switch (true) {
+      case this.state.volume === 0:
+        this.trigger.className = "player-button player-volume-muted";
+        break;
+      case this.state.volume < 0.5:
+        this.trigger.className = "player-button player-volume-low";
+        break;
+      case this.state.volume >= 0.5:
+        this.trigger.className = "player-button player-volume-high";
+        break;
+      default:
+        break;
+    }
   }
 
   componentDidMount() {
@@ -24,7 +39,11 @@ class VolumeControls extends React.Component {
     this.trigger.addEventListener("mouseenter", (e) =>
       this.getSliderDimensions(e)
     );
-    document.addEventListener("mouseup", (e) => this.dragEnd(e));
+    document.addEventListener("mouseup", (e) => {
+      if (this.selecting){
+        this.dragEnd(e)
+      }
+    });
     document.addEventListener("mousemove", (e) => {
       if (this.selecting) {
         this.dragMove(e);
@@ -32,14 +51,14 @@ class VolumeControls extends React.Component {
     });
   }
 
-  getSliderDimensions(e){
+  getSliderDimensions(e) {
     let slider = this.sliderAxis.getBoundingClientRect();
     this.top = slider.top;
     this.bottom = slider.bottom;
     this.range = this.bottom - this.top;
   }
 
-  convertPositionToVolume(pos){
+  convertPositionToVolume(pos) {
     let tempVol = (this.bottom - pos) / this.range;
     tempVol = tempVol.toFixed(2);
     if (tempVol < 0) {
@@ -49,12 +68,12 @@ class VolumeControls extends React.Component {
     }
     return tempVol;
   }
-  
+
   dragStart(e) {
     this.selecting = true;
-    this.sliderBox.classList.add("active")
+    this.sliderBox.classList.add("active");
     let tempVol = this.convertPositionToVolume(e.clientY);
-    this.setState({volume: tempVol});
+    this.setState({ volume: tempVol });
   }
 
   dragMove(e) {
@@ -63,13 +82,35 @@ class VolumeControls extends React.Component {
   }
 
   dragEnd(e) {
+    console.log("Drag End");
     this.selecting = false;
     this.sliderBox.classList.remove("active");
+    if (e.target.id === "volume-button"){
+      this.dragEndOnMute = true;
+    }
   }
 
+  toggelMute(e){
+    console.log("Toggle mute")
+    if (e.target.id !== "volume-button" || this.dragEndOnMute) {
+      this.dragEndOnMute = false;
+      return;
+    }
+    if (this.state.volume > 0) {
+      this.lastVolume = this.state.volume;
+      this.setState({ volume: 0 });
+    } else {
+      this.setState({ volume: this.lastVolume });
+    }
+  }
+  
   render() {
     return (
-      <div id="volume-button" className="player-volume-high player-button">
+      <div
+        id="volume-button"
+        className="player-volume-high player-button"
+        onClick={(e) => this.toggelMute(e)}
+      >
         <div id="volume-slider-box" onMouseDown={(e) => this.dragStart(e)}>
           <div id="volume-slider-axis">
             <div id="volume-slider-axis-filled" />
