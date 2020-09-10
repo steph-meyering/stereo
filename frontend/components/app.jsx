6a5b12938ff819507d994e74f62441bdcbb8b888
Steph Meyering <stephane.meyering@gmail.com>
2020-09-10 16:53:35 -0700

    make /upload a ProtectedRoute to prevent unauthenticated users from uploading songs

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "./navbar/navbar_container";
-import SignupFormContainer from "./session_form/signup_form_container";
-import LoginFormContainer from "./session_form/login_form_container";
 import { Switch, Route, Redirect, Router } from "react-router-dom";
-import { AuthRoute } from "../util/route_util";
+import { ProtectedRoute } from "../util/route_util";
 import ModalContainer from "./modal/modal_container";
 import SongShowContainer from "./songs/song_show_container";
 import SongIndexContainer from "./songs/song_index_container";
-import TestContainer from "./dev_test/testing_temp";
+// import TestContainer from "./dev_test/testing_temp";
 import UserShow from "./users/user_show_container";
 import NotFound from "./errors/404";
 import SongFormContainer from "./songs/song_form_container";
@@ -26,12 +24,12 @@ const App = () => (
         <Route exact path="/users/:userId" component={UserShow} />
         <Route exact path="/songs/:songId" component={SongShowContainer} />
         <Route exact path="/songs" component={SongIndexContainer} />
-        <Route exact path="/upload" component={SongFormContainer} />
+        <ProtectedRoute exact path="/upload" component={SongFormContainer} />
         <Route exact path="/404" component={NotFound} />
         <Redirect path="*" to="/404" />
       </Switch>
     </div>
-    <PlayControlsContainer/>
+    <PlayControlsContainer />
   </div>
 );
 
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { withRouter, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { Route, Redirect, withRouter } from "react-router-dom";
 
+// renders component if logged out, otherwise redirects to the root url
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
-    <Route
-        path={path}
-        exact={exact}
-        render={props =>
-            !loggedIn ? <Component {...props} /> : <Redirect to="/" />
-        }
-    />
+  <Route
+    path={path}
+    exact={exact}
+    render={(props) =>
+      !loggedIn ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
 );
 
-const mapStateToProps = state => {
-    return { loggedIn: Boolean(state.session.id) };
-};
-
-export const AuthRoute = withRouter(
-    connect(
-        mapStateToProps,
-        null
-    )(Auth)
+// renders component if logged in, otherwise redirects to the login page
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact ? true : false}
+    render={(props) =>
+      loggedIn ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
 );
+
+// access the Redux state to check if the user is logged in
+const mapStateToProps = (state) => ({ loggedIn: Boolean(state.session.id) });
+
+// connect Auth to the redux state
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+
+// connect Protected to the redux state
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
