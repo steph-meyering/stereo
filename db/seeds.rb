dba0a51ba5acf6f37cc72d2c41122bcf137372ed
Stephane Meyering <stephane.meyering@gmail.com>
2026-01-16 15:20:41 -0800

    chore: make seeds idempotent for Render deploys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,195 +1,149 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 # gem needed to process URI instead of filepaths
-require 'open-uri'
+require "open-uri"
 
-User.destroy_all
-Song.destroy_all
+# Optional reset (useful for local dev only)
+if ENV["RESET_SEEDS"] == "true"
+  Comment.destroy_all
+  Song.destroy_all
+  User.destroy_all
+end
 
-guest = User.create!(
-    username: "Demo User",
-    password: "password",
-    email: "guest@email.com",
-    location: "Somewhere in the ActiveStorage Blob",
-    about: "I'm a demo user, my profile is here to show you what a user profile looks like for testing purposes. Though I am saving you the time and trouble of making your own account, you're more than welcome to create your own! :)"
+def ensure_user(attrs)
+  user = User.find_or_initialize_by(username: attrs[:username])
+  user.email = attrs[:email]
+  user.location = attrs[:location]
+  user.about = attrs[:about]
+  user.admin = attrs[:admin] || false
+  user.password = attrs[:password] if user.new_record?
+  user.save!
+  user
+end
+
+def ensure_song(artist:, title:, genre:, audio_url:, photo_url:)
+  song = Song.find_or_initialize_by(artist_id: artist.id, title: title)
+  song.genre = genre
+  song.save!
+
+  unless song.file.attached?
+    file = URI.open(audio_url)
+    song.file.attach(io: file, filename: File.basename(URI.parse(audio_url).path))
+  end
+
+  unless song.photo.attached?
+    photo = URI.open(photo_url)
+    song.photo.attach(io: photo, filename: File.basename(URI.parse(photo_url).path))
+  end
+
+  song
+end
+
+guest = ensure_user(
+  username: "Demo User",
+  password: "password",
+  email: "guest@email.com",
+  location: "Somewhere in the ActiveStorage Blob",
+  about: "I'm a demo user, my profile is here to show you what a user profile looks like for testing purposes. Though I am saving you the time and trouble of making your own account, you're more than welcome to create your own! :)"
 )
 
-steph = User.create!(
-    username: "Steph Meyering",
-    password: "password",
-    email: "stephane.meyering@gmail.com",
-    location: "San Francisco",
-    about: "I built this",
-    admin: true
+steph = ensure_user(
+  username: "Steph Meyering",
+  password: "password",
+  email: "stephane.meyering@gmail.com",
+  location: "San Francisco",
+  about: "I built this",
+  admin: true
 )
 
-# create a Song instance
-song1 = Song.new(
-    artist_id: steph.id, 
-    title:"Apashe - Work (feat. Young Buck)", 
-    genre: "bass")
+# Optional: include this track only when explicitly requested
+if ENV["INCLUDE_SONG1"] == "true"
+  ensure_song(
+    artist: steph,
+    title: "Apashe - Work (feat. Young Buck)",
+    genre: "bass",
+    audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Apashe+-+Work+(feat.+Young+Buck).mp3",
+    photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/apashe_cover.jpg"
+  )
+end
 
-# open the corresponding audio file from S3
-file1 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Apashe+-+Work+(feat.+Young+Buck).mp3")
+ensure_song(
+  artist: guest,
+  title: "dontcry - redbone",
+  genre: "lo-fi chill",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/dontcry+-+redbone.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/dontcry+-+redbone.jpg"
+)
 
-# attach the audio file to the Song instance
-song1.file.attach(io: file1, filename: "file1")
+ensure_song(
+  artist: steph,
+  title: "B l o m s t - Chill Rain",
+  genre: "chillout",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Blomst-ChillRain.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/blomst-chillrain.jpg"
+)
 
-photo1 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/apashe_cover.jpg")
-song1.photo.attach(io: photo1, filename: "photo1")
+ensure_song(
+  artist: steph,
+  title: "Foster The People - Pumped Up Kicks",
+  genre: "indie pop",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/FosterThePeople-PumpedUpKicks.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/FosterThePeople-PumpedUpKicks.jpg"
+)
 
-# persist song to database
+ensure_song(
+  artist: steph,
+  title: "Gramatik - East Coast",
+  genre: "electronica",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Gramatik-EastCoast.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/Gramatik-EastCoast.jpg"
+)
 
-# removing this specific song from production seed data
-# song1.save!
+ensure_song(
+  artist: steph,
+  title: "Kill The Noise ft Mija - Salvation",
+  genre: "bass",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/KillTheNoise-Salvation.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/KillTheNoise-Salvation.jpg"
+)
 
-# ================================ 
-song2 = Song.new(
-    artist_id: guest.id, 
-    title:"dontcry - redbone", 
-    genre: "lo-fi chill")
+ensure_song(
+  artist: steph,
+  title: "Kupla - Feathers",
+  genre: "chill",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/kupla-feathers.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/kupla-feathers.jpg"
+)
 
-file2 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/dontcry+-+redbone.mp3")
-song2.file.attach(io: file2, filename: "file2")
+ensure_song(
+  artist: steph,
+  title: "Letherette - CartoonHaunt",
+  genre: "experimental",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Letherette-CartoonHaunt.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/Letherette-CartoonHaunt.jpg"
+)
 
-photo2 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/dontcry+-+redbone.jpg")
-song2.photo.attach(io: photo2, filename: "photo2")
+ensure_song(
+  artist: steph,
+  title: "Saib - Piano bar",
+  genre: "lofi hip-hop",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/saib-pianobar.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/saib-pianobar.jpg"
+)
 
-song2.save!
+ensure_song(
+  artist: steph,
+  title: "tails - Misted",
+  genre: "chillout",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/tails-Misted.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/tails-Misted.jpg"
+)
 
-# ================================
-song3 = Song.new(
-    artist_id: steph.id, 
-    title:"B l o m s t - Chill Rain", 
-    genre: "chillout")
-
-file3 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Blomst-ChillRain.mp3")
-song3.file.attach(io: file3, filename: "file3")
-
-photo3 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/blomst-chillrain.jpg")
-song3.photo.attach(io: photo3, filename: "photo3")
-song3.save!
-
-# ================================
-song4 = Song.new(
-    artist_id: steph.id, 
-    title:"Foster The People - Pumped Up Kicks", 
-    genre: "indie pop")
-
-file4 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/FosterThePeople-PumpedUpKicks.mp3")
-song4.file.attach(io: file4, filename: "file4")
-
-photo4 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/FosterThePeople-PumpedUpKicks.jpg")
-song4.photo.attach(io: photo4, filename: "photo4")
-song4.save!
-
-# ================================
-song5 = Song.new(
-    artist_id: steph.id, 
-    title:"Gramatik - East Coast", 
-    genre: "electronica")
-
-file5 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Gramatik-EastCoast.mp3")
-song5.file.attach(io: file5, filename: "file5")
-
-photo5 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/Gramatik-EastCoast.jpg")
-song5.photo.attach(io: photo5, filename: "photo5")
-song5.save!
-
-# ================================
-song6 = Song.new(
-    artist_id: steph.id, 
-    title:"Kill The Noise ft Mija - Salvation", 
-    genre: "bass")
-
-file6 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/KillTheNoise-Salvation.mp3")
-song6.file.attach(io: file6, filename: "file6")
-
-photo6 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/KillTheNoise-Salvation.jpg")
-song6.photo.attach(io: photo6, filename: "photo6")
-song6.save!
-
-# ================================
-song7 = Song.new(
-    artist_id: steph.id, 
-    title:"Kupla - Feathers", 
-    genre: "chill")
-
-file7 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/kupla-feathers.mp3")
-song7.file.attach(io: file7, filename: "file7")
-
-photo7 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/kupla-feathers.jpg")
-song7.photo.attach(io: photo7, filename: "photo7")
-song7.save!
-
-# ================================
-song8 = Song.new(
-    artist_id: steph.id, 
-    title:"Letherette - CartoonHaunt", 
-    genre: "experimental")
-
-file8 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/Letherette-CartoonHaunt.mp3")
-song8.file.attach(io: file8, filename: "file8")
-
-photo8 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/Letherette-CartoonHaunt.jpg")
-song8.photo.attach(io: photo8, filename: "photo8")
-song8.save!
-
-# ================================
-song9 = Song.new(
-    artist_id: steph.id, 
-    title:"Saib - Piano bar", 
-    genre: "lofi hip-hop")
-
-file9 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/saib-pianobar.mp3")
-song9.file.attach(io: file9, filename: "file9")
-
-photo9 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/saib-pianobar.jpg")
-song9.photo.attach(io: photo9, filename: "photo9")
-song9.save!
-
-# ================================
-song10 = Song.new(
-    artist_id: steph.id, 
-    title:"tails - Misted", 
-    genre: "chillout")
-
-file10 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/tails-Misted.mp3")
-song10.file.attach(io: file10, filename: "file10")
-
-photo10 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/tails-Misted.jpg")
-song10.photo.attach(io: photo10, filename: "photo10")
-song10.save!
-
-# ================================
-song11 = Song.new(
-    artist_id: steph.id, 
-    title:"Wax Tailor - Que Sera", 
-    genre: "electronica")
-
-file11 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/music/WaxTailor-QueSera.mp3")
-song11.file.attach(io: file11, filename: "file11")
-
-photo11 = open("https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/WaxTailor-QueSera.jpg")
-song11.photo.attach(io: photo11, filename: "photo11")
-song11.save!
-
-# # ======TEMPLATE==========================
-# # ================================
-# # song@ = Song.new(
-# #     artist_id: steph.id, 
-# #     title:"", 
-# #     genre: "")
-
-# # file@ = open("")
-# # song.file.attach(io: file, filename: "file")
-
-# # photo = open("")
-# # song.photo.attach(io: photo, filename: "photo")
+ensure_song(
+  artist: steph,
+  title: "Wax Tailor - Que Sera",
+  genre: "electronica",
+  audio_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/music/WaxTailor-QueSera.mp3",
+  photo_url: "https://stereo-seeds.s3-us-west-1.amazonaws.com/album-art/WaxTailor-QueSera.jpg"
+)
